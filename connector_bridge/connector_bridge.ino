d20739a04a8ac1d3630608943d02974498c1854d
meltanrm <89637384+meltanrm@users.noreply.github.com>
2023-01-25 16:33:21 +0000

    Added code for connector bridge

---
+++ connector_bridge/connector_bridge.ino
@@ -0,0 +1,46 @@
+
+//**********************************************************************************
+// Code to operate stepper motor controlling connector bridge
+// connector bridge should rotate 90˚ and back every couple of seconds to 
+// limit the number of ants allowed on the bridge set up 
+//**********************************************************************************
+
+// Define constants 
+const int dirPin = 2;  // Direction
+const int stepPin = 5; // Step ~
+
+// Motor steps per rotation (full rotation = 200) 
+const int STEPS_PER_REV = 50; // 90 degrees
+
+void setup() {
+// Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+
+void loop() {
+  // rotates 90˚ clockwise
+  digitalWrite(dirPin,HIGH); 
+
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(4000); // note: at delay of 2000, my boi got flung off the stick onto the ground 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(4000); 
+  }
+
+  // waits for 8 seconds
+  delay(8000);
+
+  // rotates 90˚ anticlockwise
+  digitalWrite(dirPin,LOW); 
+  
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(4000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(4000); 
+  }
+  
+  delay(8000);
+}
